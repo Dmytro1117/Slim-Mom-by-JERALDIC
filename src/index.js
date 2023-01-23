5662dace7dc632cfa97e9dabdd6f7a7a0ef4a53f
Dmytro <111579384+Dmytro1117@users.noreply.github.com>
2023-01-24 01:25:41 +0200

    json

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="slim-mom">
+    <BrowserRouter basename="Slim-Mom-by-JERALDIC">
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <App />
